6a63d55159a94c072116fd542f323f22e54702df
jimmoffet <jimmoffet@gmail.com>
2019-04-28 18:27:54 -0400

    clean up

--- distancematrix.py
+++
@@ -1,6 +0,0 @@
-'https://maps.googleapis.com/maps/api/distancematrix/json'
-+'?units=imperial'
-+'&mode=walking'
-+'&origins=42.372608,-71.106027'
-+'&destinations=42.370666,-71.108800'
-+'&key=AIzaSyDtbzOO7z-9NcDCHTJoj9lJdI59kNKPxYo'
